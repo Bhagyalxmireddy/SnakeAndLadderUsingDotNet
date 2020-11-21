2d74130201a6b8525dca477ff52e83e675627659
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 23:43:41 +0530

    [Bhagyalaxmi]ADDED:Added code to print welcome msg

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadderProgram
+{
+    class SnakeAndLadder
+    {
+         int STARTINGPOSITION = 0;
+         int LADDER = 1;
+         int SNAKE = 2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SnakeAndLadder Program");
+        }
+    }
+}
