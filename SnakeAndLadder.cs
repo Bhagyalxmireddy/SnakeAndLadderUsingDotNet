50578fff29d04857b860fd5be257b2cabd9eabef
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 23:56:51 +0530

    [Bhagyalaxmi]ADDED:Added code to generate a random value

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -7,9 +7,18 @@ namespace SnakeAndLadderProgram
          int STARTINGPOSITION = 0;
          int LADDER = 1;
          int SNAKE = 2;
+         int PLAYER = 0;
+            public static void gameplayed()
+             {
+                Console.WriteLine("welcome to single player game");
+                Random random = new Random();
+                int dice = (random.Next(0, 6) + 1);
+                Console.WriteLine("The Dice value is : " + dice);
+             }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SnakeAndLadder Program");
+            gameplayed();
         }
     }
 }
